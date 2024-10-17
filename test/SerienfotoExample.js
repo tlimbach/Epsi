19d5d68a2b7b1a651632eb100ace4b7f05e793ae
tlimbach <49364223+tlimbach@users.noreply.github.com>
2024-10-17 16:23:03 +0200

    develop

--- test/SerienfotoExample.js
+++ test/SerienfotoExample.js
@@ -13,6 +13,7 @@ function startVideoStream(facingMode = 'environment') {
     })
     .then(stream => {
         video.srcObject = stream;
+        video.play();
 
         // Alle 100ms ein Foto machen
         setInterval(capturePhoto, 100);
@@ -23,18 +24,23 @@ function startVideoStream(facingMode = 'environment') {
 }
 
 function capturePhoto() {
-    // Setze Canvas Größe entsprechend des Videos
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
+    // Vergewissere dich, dass das Video noch läuft
+    if (video.videoWidth > 0 && video.videoHeight > 0) {
+        // Setze die Canvas-Größe entsprechend dem Video
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
 
-    // Zeichne das aktuelle Video-Bild auf das Canvas
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+        // Zeichne das aktuelle Video-Bild auf das Canvas
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-    // Konvertiere Canvas in ein Bild
-    const imgData = canvas.toDataURL('image/png');
+        // Konvertiere Canvas in ein Bild
+        const imgData = canvas.toDataURL('image/png');
 
-    // Setze das Bild als Quelle für das <img> Tag
-    photoElement.src = imgData;
+        // Setze das Bild als Quelle für das <img> Tag
+        photoElement.src = imgData;
+    } else {
+        console.log("Video is not available yet");
+    }
 }
 
 // Starte den Video-Stream
