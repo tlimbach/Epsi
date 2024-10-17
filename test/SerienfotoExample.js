47acf594d50c8e5d1ee10865ee6f50f70dbd5d1b
tlimbach <49364223+tlimbach@users.noreply.github.com>
2024-10-18 00:55:03 +0200

    develop

--- test/SerienfotoExample.js
+++ test/SerienfotoExample.js
@@ -105,7 +105,7 @@ function processWithTesseract(imageData) {
     const startTime = performance.now();
 
     Tesseract.recognize(imageData, 'deu', {
-        tessedit_char_whitelist: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
+        tessedit_char_whitelist: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789€.,%gGkKmL+-',
         tessedit_pageseg_mode: Tesseract.PSM.AUTO, // Automatische Segmentierung für mehrere Textblöcke
         logger: m => console.log(m)
     })
